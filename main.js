376ee6dc19a531eab4d640a692e8187bd4405a6a
donny-b <88580348+donny-b@users.noreply.github.com>
2022-07-21 00:06:30 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+class DigitalClock {
+    constructor(element) {
+        this.element = element;
+    }
+
+    start() {
+        this.update();
+
+        setInterval(() => {
+            this.update();
+        }, 500);
+    }
+
+    update() {
+        const parts = this.getTimeParts();
+        const minuteFormatted = parts.minute.toString().padStart(2, "0");
+        const secondFormatted = parts.second.toString().padStart(2, "0");
+        const timeFormatted = `${parts.hour}:${minuteFormatted}:${secondFormatted}`;
+        const amPm = parts.isAm ? "AM" : "PM";
+
+        this.element.querySelector(".clock-time").textContent = timeFormatted;
+        this.element.querySelector(".clock-ampm").textContent = amPm;
+
+
+        console.log(timeFormatted);
+    }
+
+    getTimeParts() {
+        const now = new Date();
+
+        return {
+            hour: now.getHours() % 12 || 12,
+            minute: now.getMinutes(),
+            second: now.getSeconds(),
+            isAm: now.getHours() < 12
+        };
+    }
+}
+
+const clockElement = document.querySelector(".clock");
+const clockObject = new DigitalClock(clockElement);
+
+clockObject.start();
+
